8d372150e29e881023745d0b84cf6472981933f5
Corvax <vlvete1990@yandex.ru>
2024-04-25 21:17:26 +0300

    feature: add product controller

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Patch,
+} from '@nestjs/common';
+import { ProductModel } from './product.model';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+  @Post('create')
+  async create(@Body() dto: Omit<ProductModel, '_id'>) {
+    console.log(dto);
+  }
+
+  @Get(':id')
+  async get(@Param('id') id: string) {}
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {}
+
+  @Patch(':id')
+  async patch(@Param('id') id: string, @Body() dto: ProductModel) {}
+}
