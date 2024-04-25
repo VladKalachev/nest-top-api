d5aaf94438dbe151884eb24a34a71eb1053eaa4b
Corvax <vlvete1990@yandex.ru>
2024-04-26 01:54:54 +0300

    feature: add top page mode;

--- src/top-page/top-page.model.ts
+++ src/top-page/top-page.model.ts
@@ -1,3 +1,6 @@
+import { prop, index } from '@typegoose/typegoose';
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+
 export enum TopLevelCategory {
   Courses,
   Services,
@@ -6,30 +9,67 @@ export enum TopLevelCategory {
 }
 
 export class HhData {
+  @prop()
   count: number;
+
+  @prop()
   juniorSalary: number;
+
+  @prop()
   middleSalary: number;
+
+  @prop()
   seniorSalary: number;
+
+  @prop()
   updatedAt: Date;
 }
 
 export class TopPageAdvantage {
+  @prop()
   title: string;
+
+  @prop()
   description: string;
 }
 
-export class TopPageModel {
-  _id: string;
+export interface TopPageModel extends Base {}
+
+@index({ '$**': 'text' })
+export class TopPageModel extends TimeStamps {
+  @prop({ enum: TopLevelCategory })
   firstCategory: TopLevelCategory;
+
+  @prop()
   secondCategory: string;
+
+  @prop({ unique: true })
   alias: string;
+
+  @prop()
   title: string;
+
+  @prop()
   metaTitle: string;
+
+  @prop()
   metaDescription: string;
+
+  @prop()
   category: string;
+
+  @prop({ type: () => HhData })
   hh?: HhData;
+
+  @prop({ type: () => [TopPageAdvantage] })
   advantages?: TopPageAdvantage[];
+
+  @prop()
   seoText?: string;
+
+  @prop()
   tagsTitle: string;
+
+  @prop({ type: () => [String] })
   tags: string[];
 }
