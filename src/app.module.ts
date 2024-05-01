dd75a65eef88fe6690cc6fdc65fc35cbd0e0744a
Corvax <vlvete1990@yandex.ru>
2024-05-01 19:55:33 +0300

    feature: add sitemap

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { TypegooseModule } from 'nestjs-typegoose';
 import { getMongoConfig } from './configs/mongo.config';
 import { HhModule } from './hh/hh.module';
 import { FilesModule } from './files/files.module';
+import { SitemapModule } from './sitemap/sitemap.module';
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { FilesModule } from './files/files.module';
     ReviewModule,
     HhModule,
     FilesModule,
+    SitemapModule,
   ],
   controllers: [],
   providers: [],
